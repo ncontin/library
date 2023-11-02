14069f5c3ca943949c2cc5fd62eccd1cc1ad2862
Nicolo Contin <95549154+ncontin@users.noreply.github.com>
2023-11-02 17:44:32 +0100

    refactor code with classes

--- script.js
+++ script.js
@@ -13,6 +13,81 @@ const myLibrary = [
     },
 ];
 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+class Library {
+    constructor(books) {
+        this.books = books;
+    }
+
+    addBookToLibrary() {
+        const title = bookTitleInput?.value;
+        const author = bookAuthorInput?.value;
+        const pages = bookPagesInput.value;
+        const read = bookReadInput.checked;
+        const newBook = new Book(title, author, pages, read);
+        this.books.unshift(newBook);
+        console.log(this.books);
+    }
+
+    createCard(book) {
+        let card = document.createElement("div");
+        card.className = "book-card";
+
+        let title = document.createElement("h2");
+        title.textContent = book.title;
+        card.appendChild(title);
+
+        let author = document.createElement("p");
+        author.textContent = "By " + book.author;
+        card.appendChild(author);
+
+        let pages = document.createElement("div");
+        pages.textContent = book.pages + " Pages";
+        card.appendChild(pages);
+
+        let read = document.createElement("button");
+        read.textContent = book.read ? "Read" : "Not Read";
+        read.addEventListener("click", () => {
+            book.read = !book.read;
+            read.textContent = book.read ? "Read" : "Not Read";
+        });
+
+        card.appendChild(read);
+
+        return card;
+    }
+
+    createDeleteButton(index) {
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.addEventListener("click", () => {
+            this.books.splice(index, 1);
+            this.displayBooks();
+        });
+
+        return deleteBtn;
+    }
+
+    displayBooks() {
+        library.innerHTML = "";
+        this.books.forEach((book, index) => {
+            let card = this.createCard(book);
+            let deleteBtn = this.createDeleteButton(index);
+
+            card.appendChild(deleteBtn);
+            library?.appendChild(card);
+        });
+    }
+}
+
 const bookTitleInput = document.getElementById("book-title");
 const bookAuthorInput = document.getElementById("book-author");
 const bookPagesInput = document.getElementById("book-pages");
@@ -23,72 +98,7 @@ const favDialog = document.getElementById("favDialog");
 const cancelBtn = document.getElementById("cancelBtn");
 const confirmBtn = favDialog.querySelector("#confirmBtn");
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary() {
-    const title = bookTitleInput?.value;
-    const author = bookAuthorInput?.value;
-    const pages = bookPagesInput.value;
-    const read = bookReadInput.checked;
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.unshift(newBook);
-    console.log(myLibrary);
-}
-
-function createCard(book) {
-    let card = document.createElement("div");
-    card.className = "book-card";
-
-    let title = document.createElement("h2");
-    title.textContent = book.title;
-    card.appendChild(title);
-
-    let author = document.createElement("p");
-    author.textContent = "By " + book.author;
-    card.appendChild(author);
-
-    let pages = document.createElement("div");
-    pages.textContent = book.pages + " Pages";
-    card.appendChild(pages);
-
-    let read = document.createElement("button");
-    read.textContent = book.read ? "Read" : "Not Read";
-    read.addEventListener("click", () => {
-        book.read = !book.read;
-        read.textContent = book.read ? "Read" : "Not Read";
-    });
-
-    card.appendChild(read);
-
-    return card;
-}
-
-function createDeleteButton(index) {
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", () => {
-        myLibrary.splice(index, 1);
-        displayBooks();
-    });
-
-    return deleteBtn;
-}
-
-function displayBooks() {
-    library.innerHTML = "";
-    myLibrary.forEach((book, index) => {
-        let card = createCard(book);
-        let deleteBtn = createDeleteButton(index);
-
-        card.appendChild(deleteBtn);
-        library?.appendChild(card);
-    });
-}
+const myLibraryInstance = new Library(myLibrary);
 
 newBookBtn?.addEventListener("click", () => {
     favDialog.showModal();
@@ -105,11 +115,12 @@ cancelBtn?.addEventListener("click", () => {
 confirmBtn?.addEventListener("click", (event) => {
     event.preventDefault();
     if (event.target.form.checkValidity()) {
-        addBookToLibrary();
-        displayBooks();
+        myLibraryInstance.addBookToLibrary();
+        myLibraryInstance.displayBooks();
         favDialog.close();
     } else {
         alert("Please fill out all required fields.");
     }
 });
-displayBooks();
+
+myLibraryInstance.displayBooks();
